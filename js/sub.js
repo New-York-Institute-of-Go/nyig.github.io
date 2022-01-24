c2f163b44912191952a313e7bee87aa6a8344dec
Ryan Li <77685035+liryan1@users.noreply.github.com>
2022-01-24 15:27:30 -0500

    add google recaptcha

--- js/sub.js
+++ js/sub.js
@@ -1,3 +1,4 @@
+// Automatically add form submission to Google Sheets
 const subscribeURL = "https://script.google.com/macros/s/AKfycbz3VAGBDh9xiZ0ScPEcMW5xZx3VLghr0tnjfpn7VbQI8W0-HS5j/exec"
 const subForm = document.forms['subscribe']
 
